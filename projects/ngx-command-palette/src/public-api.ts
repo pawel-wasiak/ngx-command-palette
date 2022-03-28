6743a3f60ce34334fbc2b2077a7e9d9fc162dc33
Paweł Wasiak <pawel3317@gmail.com>
2022-03-28 17:41:42 +0200

    Update public api

--- projects/ngx-command-palette/src/public-api.ts
+++ projects/ngx-command-palette/src/public-api.ts
@@ -5,8 +5,14 @@
 export * from './lib/command-palette.service';
 export * from './lib/command-palette.component';
 export * from './lib/command-palette.module';
+
 export * from './lib/content/command-palette-content.component';
 export * from './lib/header/command-palette-header.component';
-export * from './lib/services/command-palette-shortcuts.service';
+
+export * from './lib/model/command-item';
+export * from './lib/model/command-item-group';
+export * from './lib/model/command-palette-config';
+
 export * from './lib/services/command-palette-creator-service';
-export * from 'projects/ngx-command-palette/src/lib/services/command-palette-group-storage.service';
+export * from './lib/services/command-palette-group-storage.service';
+export * from './lib/services/command-palette-shortcuts.service';
